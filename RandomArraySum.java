b40ed5d375b97a9fbcd68685f7dc48a4789e8bde
Serkan HELVACIOGLU <serkanhelvacioglu@outlook.com>
2017-10-17 16:07:07 +0300

    Update RandomArraySum.java

--- RandomArraySum.java
+++ RandomArraySum.java
@@ -4,6 +4,16 @@ import java.util.Random;
 
 public class Arrays {
 	
+	
+		public static void list(int[] array, int length){
+			
+			for(int i=0; i<5;i++)
+			{
+				System.out.print(array[i]+" ");
+			}
+			System.out.println();
+		}
+	
 		public static void main(String[] args)
 		{
 			/*int[] array1=new int[5];
@@ -21,20 +31,27 @@ public class Arrays {
 			{
 				
 				array1[i]=rnd.nextInt(11)+10;
+				array2[i]=rnd.nextInt(11)+10;
+				result[i]=array1[i]+array2[i];
 				
 			}
+			
+			
+			list(array1,array1.length);
+			list(array2,array2.length);	//İnstead of below for
+			list(result,result.length);
+			
+			/*for(int i=0; i<5;i++)
+			{
+				System.out.print(array1[i]+" ");
+			}
+			System.out.println();
 			for(int i=0; i<5;i++)
 			{
 				System.out.print(array1[i]+" ");
 			}
 			System.out.println();
 			
-			for(int i=0;i<5;i++)
-			{
-				
-				array2[i]=rnd.nextInt(11)+10;
-				
-			}
 			for(int i=0; i<5;i++)
 			{
 				System.out.print(array2[i]+" ");
@@ -43,9 +60,8 @@ public class Arrays {
 			
 			for(int i=0; i<5;i++)
 			{
-				result[i]=array1[i]+array2[i];
 				System.out.print(result[i]+" ");
-			}
+			}*/
 			
 		}
 

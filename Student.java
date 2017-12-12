ceb392cde83752261e98fb4d924e0f1577c2d51d
Serkan HELVACIOGLU <serkanhelvacioglu@outlook.com>
2017-12-12 16:17:47 +0300

    Create Student.java
    It depends on Enrollment.java

---
+++ Student.java
@@ -0,0 +1,20 @@
+package pack1;
+
+public class Student {
+
+	String name="";
+	String Surname="";
+	String Department="";
+	
+	public Student(String name, String surname, String department) {
+		super();
+		this.name = name;
+		this.Surname = surname;
+		this.Department = department;
+	}
+	
+	
+	
+	
+
+}
